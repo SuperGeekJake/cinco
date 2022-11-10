0a9a211e8af849015f83f4d75b542218264072b3
Jacob Williams <supergeekjake@users.noreply.github.com>
2022-11-09 17:13:08 -0800

    chore: Use Board component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,131 +1,23 @@
-import {
-  Component,
-  createEffect,
-  createMemo,
-  onCleanup,
-  For,
-  Show,
-} from "solid-js";
-import createPanZoom from "panzoom";
+import { Component, For, Show } from "solid-js";
 
-import { PlayerID, TokenID, MAX_TOKENS, INIT_STATE, createGame } from "./game";
+import { INIT_STATE, createGame } from "./game";
+import { Board } from "./Board";
+import { CPU_ID } from "./ai";
 
 export const App: Component = () => {
-  let boardElement: HTMLDivElement;
-  let currentTarget: TokenID | undefined;
-  let isTransforming: boolean = false;
-
   const { state, tokenPlaced, startedGame, resettedGame } = createGame({
     ...INIT_STATE,
     playOrder: [CLIENT_ID, CLIENT_ID2],
   });
 
-  const isBoardActive = createMemo(
-    () => state.status !== "LIVE" // ||
-    // state.playOrder[state.currentPlayer] !== CLIENT_ID
-  );
-
-  const getPossibleMoves = createMemo(() => {
-    const currentBoard = board.slice();
-    Object.keys(state.tokens).forEach((value) => {
-      delete currentBoard[value];
-    });
-    return currentBoard;
-  });
-
-  const getPlayerOrder = createMemo(() => {
-    return state.playOrder.reduce<Record<PlayerID, number>>(
-      (result, playerID, index) => {
-        result[playerID] = index;
-        return result;
-      },
-      {}
-    );
-  });
-
-  const getTokenClasses = (tokenID: number) => {
-    const playerOrderID = getPlayerOrder()[state.tokens[tokenID]];
-    return tokenClasses[playerOrderID];
-  };
-
-  const handleMouseDown = (tokenID: number) => {
-    currentTarget = tokenID;
-  };
-
-  const handleMouseUp = () => {
-    if (isTransforming) return;
-    if (isBoardActive() || state.tokens[currentTarget] !== undefined) return;
-    tokenPlaced(currentTarget);
-    currentTarget = undefined;
-  };
-
-  const filterMouseEvent =
-    (callback: (tokenID: number) => void, tokenID: number) =>
-    (event: MouseEvent) => {
-      if (event.button !== 0) return;
-      callback(tokenID);
-    };
-
-  createEffect((prevCurrentPlayer) => {
-    let timeoutID = null;
-    if (
-      state.status === "LIVE" &&
-      state.currentPlayer !== prevCurrentPlayer &&
-      state.playOrder[state.currentPlayer] === CPU_ID
-    ) {
-      timeoutID = setTimeout(() => {
-        const cpuMove = getRandomIndex(getPossibleMoves());
-        tokenPlaced(cpuMove);
-        timeoutID = null;
-      }, 1000);
-    }
-
-    onCleanup(() => {
-      if (!timeoutID) return;
-      clearTimeout(timeoutID);
-    });
-
-    return state.currentPlayer;
-  }, state.currentPlayer);
-
-  createEffect(() => {
-    const panzoom = createPanZoom(boardElement, {
-      bounds: true,
-      boundsPadding: 1,
-      maxZoom: 4,
-      minZoom: 1,
-      zoomSpeed: 0.4,
-    });
-
-    panzoom.on("panstart", () => {
-      isTransforming = true;
-    });
-
-    panzoom.on("panend", () => {
-      isTransforming = false;
-    });
-
-    return () => {
-      panzoom.dispose();
-    };
-  });
-
   return (
     <main class="grid grid-cols-6 h-screen">
       <div class="col-span-full lg:col-span-4 order-2 aspect-square w-full max-w-4xl">
-        <div ref={boardElement} class="grid grid-cols-19 gap-1 p-1">
-          <For each={board}>
-            {(tokenID) => (
-              <button
-                class={`block w-full relative aspect-square m-0 border-none rounded-sm p-0 ${getTokenClasses(
-                  tokenID
-                )}`}
-                onMouseDown={filterMouseEvent(handleMouseDown, tokenID)}
-                onMouseUp={filterMouseEvent(handleMouseUp, tokenID)}
-              />
-            )}
-          </For>
-        </div>
+        <Board
+          tokens={state.tokens}
+          active={state.status === "LIVE"}
+          onPlaceToken={tokenPlaced}
+        />
       </div>
       <For each={state.playOrder}>
         {(playerID, getIndex) => (
@@ -153,25 +45,11 @@ export const App: Component = () => {
   );
 };
 
-const tokenClasses = {
-  undefined: "bg-light opacity-5 hover:opacity-25",
-  0: "bg-red",
-  1: "bg-blue",
-  2: "bg-green",
-  3: "bg-yellow",
-};
-
 const CLIENT_ID = "__PLAYER_01__";
 const CLIENT_ID2 = "__PLAYER_02__";
-const CPU_ID = "__CPU_PLAYER_02__";
 
 const nameMessages = {
   [CLIENT_ID]: "Player 1",
   [CLIENT_ID2]: "Player 2",
   [CPU_ID]: "Computer 2",
 };
-
-const board = Array.from({ length: MAX_TOKENS }, (_, i) => i);
-
-const getRandomIndex = <A extends any[]>(arr: A) =>
-  arr[Math.floor(Math.random() * arr.length)];
