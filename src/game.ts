5a9811406e9f636b41c98a16a1323eb9a11f8edd
Jacob Williams <supergeekjake@users.noreply.github.com>
2022-11-09 17:11:41 -0800

    chore: Update board state to use order number
    Easier for component logic, about the same for game logic

--- src/game.ts
+++ src/game.ts
@@ -1,8 +1,9 @@
-import { createStore, produce } from "solid-js/store";
+import { createEffect } from "solid-js";
+import { createStore, produce, unwrap } from "solid-js/store";
 
 export type PlayerID = string;
 export type TokenID = number;
-export type Tokens = Record<TokenID, PlayerID>;
+export type Tokens = Record<TokenID, number>;
 export type Captures = Record<PlayerID, number>;
 export type PlayOrder = PlayerID[];
 export type Status = "PRE" | "LIVE" | "POST";
@@ -32,8 +33,11 @@ export const INIT_STATE: State = {
   tokens: {},
 };
 
+export const board = Array.from({ length: MAX_TOKENS }, (_, i) => i);
+
 export const createGame = (initState: State = INIT_STATE) => {
   const [state, setState] = createStore(initState);
+  createEffect(() => console.log(JSON.stringify(state, null, 2)));
   return {
     state,
     playerJoined: (newPlayer: PlayerID) =>
@@ -71,11 +75,11 @@ export const createGame = (initState: State = INIT_STATE) => {
       setState(
         produce<State>((s) => {
           const playerID = s.playOrder[s.currentPlayer];
-          s.tokens[tokenID] = playerID;
+          s.tokens[tokenID] = s.currentPlayer;
           s.currentPlayer = (s.currentPlayer + 1) % s.playOrder.length;
 
           // Update number of captures
-          getCaptures(s.tokens, playerID, tokenID).forEach(
+          getCaptures(s.tokens, s.currentPlayer, tokenID).forEach(
             ([tokenID1, tokenID2]) => {
               s.captures[playerID] += 1;
               delete s.tokens[tokenID1];
@@ -86,7 +90,7 @@ export const createGame = (initState: State = INIT_STATE) => {
           // End game if token caused victory
           if (
             s.captures[playerID] >= VICTORY_COUNT ||
-            hasRowVictory(s.tokens, playerID, tokenID)
+            hasRowVictory(s.tokens, s.currentPlayer, tokenID)
           ) {
             s.status = "POST";
             s.gameover = { winner: playerID };
@@ -128,11 +132,11 @@ type Getter = ReturnType<ReturnType<typeof getAdjancentToken>>;
 
 const getIsRow =
   (rowCount: number, minRowIndex: number, maxRowIndex: number) =>
-  (getter: Getter, playerID: PlayerID) => {
+  (getter: Getter, playerOrderId: number) => {
     let count = 0;
     let position = minRowIndex;
     while (true) {
-      count = getter(position) === playerID ? count + 1 : 0;
+      count = getter(position) === playerOrderId ? count + 1 : 0;
       const isRow = count >= rowCount;
       if (isRow || position >= maxRowIndex) return isRow;
       position++;
@@ -141,7 +145,7 @@ const getIsRow =
 
 const getHasRow =
   (rowCount: number) =>
-  (board: Tokens, playerID: PlayerID, tokenID: TokenID) => {
+  (board: Tokens, playerOrderId: number, tokenID: TokenID) => {
     const maxRowIndex = rowCount - 1;
     const minRowIndex = -maxRowIndex;
     const isRow = getIsRow(rowCount, minRowIndex, maxRowIndex);
@@ -151,7 +155,7 @@ const getHasRow =
     // loop through the 4 row directions
     for (let d = 0; d < DIRECTIONS.length; d++) {
       const getTokenForPosition = getTokenForDirection(d);
-      result = isRow(getTokenForPosition, playerID);
+      result = isRow(getTokenForPosition, playerOrderId);
       if (result) break;
     }
 
@@ -160,14 +164,22 @@ const getHasRow =
 
 const hasRowVictory = getHasRow(VICTORY_COUNT);
 
-const isCapture = (getter: Getter, playerID: PlayerID, rowDirection: 1 | -1) =>
+const isCapture = (
+  getter: Getter,
+  playerOrderId: number,
+  rowDirection: 1 | -1
+) =>
   !!getter(1 * rowDirection) &&
-  getter(1 * rowDirection) !== playerID &&
+  getter(1 * rowDirection) !== playerOrderId &&
   !!getter(2 * rowDirection) &&
-  getter(2 * rowDirection) !== playerID &&
-  getter(3 * rowDirection) === playerID;
+  getter(2 * rowDirection) !== playerOrderId &&
+  getter(3 * rowDirection) === playerOrderId;
 
-const getCaptures = (board: Tokens, playerID: PlayerID, tokenID: TokenID) => {
+const getCaptures = (
+  board: Tokens,
+  playerOrderId: number,
+  tokenID: TokenID
+) => {
   const captures: [number, number][] = [];
   const getTokenForDirection = getAdjancentToken(board, tokenID);
 
@@ -178,7 +190,7 @@ const getCaptures = (board: Tokens, playerID: PlayerID, tokenID: TokenID) => {
 
     // Check for a capture in either direction within the row
     for (const rowDirection of [1, -1] as const) {
-      if (isCapture(getTokenForPosition, playerID, rowDirection)) {
+      if (isCapture(getTokenForPosition, playerOrderId, rowDirection)) {
         const tokenID1 = getAdjancentTokenID(
           tokenID,
           direction,
